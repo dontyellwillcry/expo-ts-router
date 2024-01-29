cc9c064523a1d38e756642527ed0b9210db68f8b
dontyellwillcry <papaporo@proton.me>
2024-01-29 15:03:58 -0600

    trying to get the input field to return to null after user submits message

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,21 +1,42 @@
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from "react-native";
 import { useRouter } from "expo-router";
 import { useState } from "react";
 
 interface SettingsProps {}
 
 const Settings: React.FC<SettingsProps> = () => {
+  // one useState is of type 'string' and the other is of the type arry(of strings) OR null
   const [message, setMessage] = useState<string>("");
-  const [userInput, setUserInput] = useState<string | null>(null);
+  const [userInput, setUserInput] = useState<string[] | null>(null);
   const navigation = useRouter();
 
-  const handleTextChange = (text: string) => {
-    setMessage(text);
-  };
 
+  // If the users input is null then just show the current state, else return the previous stats plus new mesage
   function submitText() {
-    setUserInput(message);
+    setUserInput((prevUserInput) => {
+      if (prevUserInput === null) {
+        return [message];
+      } else {
+        return [...prevUserInput, message];
+      }
+    });
+    // setMessage('');
+
   }
+
+  // function that takes item that should be of type string and returns a View/Text with that item
+  const renderItem = ({ item }: { item: string }) => (
+    <View>
+      <Text>{item}</Text>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
@@ -24,9 +45,12 @@ const Settings: React.FC<SettingsProps> = () => {
       <TextInput placeholder="Enter text here" onChangeText={setMessage} />
       <Button title="Submit" onPress={submitText} />
       <View>
-        <Text>
-          {userInput}
-        </Text>
+        <FlatList
+          data={userInput}
+          // Not sure what keyExtractor is doing need to research. Flatlist works even if that line is commented out
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={renderItem}
+        />
       </View>
     </View>
   );
@@ -35,6 +59,7 @@ const Settings: React.FC<SettingsProps> = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 250,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
