82d95c35a77655def7c8a6a8b7cfef0baa1117d3
dontyellwillcry <papaporo@proton.me>
2024-01-29 14:20:33 -0600

    changed the code so that text input will be displayed after hitting submit

--- app/settings.tsx
+++ app/settings.tsx
@@ -2,66 +2,32 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import { useRouter } from "expo-router";
 import { useState } from "react";
 
-// export default function Settings() {
-//   const [Message, setMessage] = useState([]);
-//   console.log(Message);
-//   const navigation = useRouter();
-//   return (
-//     <View style={styles.container}>
-//       <Text>Settings!</Text>
-//       <Button title="Go to Home!" onPress={() => navigation.replace("/")} />
-//       <TextInput
-//         placeholder={"Enter text here"}
-//         onChangeText={setMessage}
-//       ></TextInput>
-//       <Text>{Message}</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
 interface SettingsProps {}
 
 const Settings: React.FC<SettingsProps> = () => {
   const [message, setMessage] = useState<string>("");
-  const [userInput, setUserInput] = useState<string[]>([]);
+  const [userInput, setUserInput] = useState<string | null>(null);
   const navigation = useRouter();
 
   const handleTextChange = (text: string) => {
-    setUserInput(prevName => [...prevName, text]);
+    setMessage(text);
   };
-  console.log(message)
-  function submitText() {
-    console.log("Submit") 
 
+  function submitText() {
+    setUserInput(message);
   }
 
   return (
     <View style={styles.container}>
       <Text>Settings!</Text>
-      <Button
-        title="Go to Home!"
-        onPress={() => navigation.replace('/')}
-      />
-      <TextInput
-        placeholder="Enter text here"
-        onChangeText={handleTextChange}
-        // Check out onEndEditing vs onChangeText
-        // onEndEditing={handleEndEditing} 
-
-      />
-      <Text>{userInput.join(', ')}</Text>
-
-      <Button
-        title="Submit"
-        onPress={submitText}/>
+      <Button title="Go to Home!" onPress={() => navigation.replace("/")} />
+      <TextInput placeholder="Enter text here" onChangeText={setMessage} />
+      <Button title="Submit" onPress={submitText} />
+      <View>
+        <Text>
+          {userInput}
+        </Text>
+      </View>
     </View>
   );
 };
@@ -69,11 +35,10 @@ const Settings: React.FC<SettingsProps> = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
 export default Settings;
-
